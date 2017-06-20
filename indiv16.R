d53849e75ca84ef62a280e042315d970339d1e64
Jacques Bikoundou <jaqo59@gmail.com>
2017-06-19 21:08:15 -0400

    Add files via upload

---
+++ indiv16.R
@@ -0,0 +1,31 @@
+# load packages
+
+library(tidyverse)
+data_dir < - "c:/users/jaqo_000/Desktop/"
+filename <- "indiv16.csv"
+read(file.path(data_dir, indiv16),...)
+library(dplyr)
+library(indiv16)
+
+# extract democratic and republican candidates for president
+
+filter(indiv16, democratic == "clinton")
+filter(indiv16, republican == "trump") 
+
+# extract donors by party
+
+filter(indiv16, donors == "republican party")
+filter(indiv16, donors == "democratic party")
+
+# separate contributions data from the two main candidates Clinton and Trump
+
+separate(indiv16, Clinton, c("donor", "city", "state", "zip code", "place of business", "amount"), sep = "_")
+separate(indiv16, Trump, c("donor", "city", "state", "zip code", "place of business", "amount"), sep = "_")
+
+# summarise amounts raised and spent by candidates
+
+summarise(indiv16, clinton, c("receipts", "disbursements"))
+summarise(indiv16, trump, c("receipts", "disbursements"))
+
+
+
